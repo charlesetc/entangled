2abc6b46483197783bac1a5122ab15847db2c9fa
Charles Chamberlain <charles@nrwhl.xyz>
2023-06-18 02:05:29 -0400

    Dragging arrows

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,22 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
 import Vec from './vec'
 
-function Vector(pos0: Vec, vec0: Vec) {
+function Vector({ pos: pos0, vec: vec0, dragging = false }) {
   const [pos, setPos] = React.useState(pos0);
   const [vec, setVec] = React.useState(vec0);
+
+  if (dragging) {
+    const mousemove = (e: MouseEvent) => {
+      setVec(new Vec(e.clientX, e.clientY).sub(pos));
+    }
+    const mouseup = (_: MouseEvent) => {
+      document.removeEventListener('mousemove', mousemove);
+      document.removeEventListener('mouseup', mouseup);
+    }
+    document.addEventListener('mousemove', mousemove);
+    document.addEventListener('mouseup', mouseup);
+  }
+
   const arrowSize = 20;
   return (
     <>
@@ -28,7 +41,6 @@ function Vector(pos0: Vec, vec0: Vec) {
         }}
         onMouseDown={e => {
           e.preventDefault();
-
           e.stopPropagation();
           const mousemove = (e: MouseEvent) => {
             setPos(new Vec(e.clientX, e.clientY));
@@ -72,25 +84,64 @@ function Vector(pos0: Vec, vec0: Vec) {
 }
 
 function App() {
+  const [vectors, setVectors] = React.useState([
+    {
+      pos: new Vec(100, 100),
+      vec: new Vec(100, 0),
+      addedTime: 0,
+    },
+  ])
   return (
     <div>
       <h1>Entangled</h1>
       <svg
+        onMouseDown={e => {
+          e.preventDefault();
+          e.stopPropagation();
+
+          let mousemove: any, mouseup: any;
+          mousemove = (e: MouseEvent) => {
+            setVectors(vectors.concat({
+              pos: new Vec(e.clientX, e.clientY),
+              vec: new Vec(100, 0),
+              addedTime: Date.now(),
+            }));
+            document.removeEventListener('mousemove', mousemove);
+            document.removeEventListener('mouseup', mouseup);
+          };
+          mouseup = (_: MouseEvent) => {
+            document.removeEventListener('mousemove', mousemove);
+            document.removeEventListener('mouseup', mouseup);
+          };
+          document.addEventListener('mousemove', mousemove);
+          document.addEventListener('mouseup', mouseup);
+
+          // setVectors(vectors.concat({
+          //   pos: new Vec(e.clientX, e.clientY),
+          //   vec: new Vec(100, 0),
+          // }));
+        }
+        }
         style={{
           position: 'fixed',
           top: 0,
           left: 0,
           zIndex: -1,
         }}
-        onClick={() => {
-
-        }}
         width="100%"
         height="100%"
       >
-        {Vector(new Vec(150, 170), new Vec(40, -40))}
+        {vectors.map(({ pos, vec }, i) => (
+
+          <Vector
+            key={i}
+            pos={pos}
+            vec={vec}
+            dragging={Date.now() - 1000 < vectors[i].addedTime}
+          />
+        ))}
       </svg>
-    </div>
+    </div >
   );
 }
 
